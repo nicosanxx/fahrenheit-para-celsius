981adc2695a5e7142e2a60c01984e661828857ed
nico <leonardo.cruz1506@gmail.com>
2022-05-29 19:39:40 -0300

    fahrenheit_para_celsius

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace fahrenheit_para_celsius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_f = new System.Windows.Forms.TextBox();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ler uma temperatura em graus Fahrenheit \r\ne apresentá-la convertida em graus Cels" +
+    "ius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FAHRENHEIT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(225, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CELSIUS";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(115, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CONVERTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_f
+            // 
+            this.txt_f.Location = new System.Drawing.Point(34, 115);
+            this.txt_f.Name = "txt_f";
+            this.txt_f.Size = new System.Drawing.Size(100, 23);
+            this.txt_f.TabIndex = 4;
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(213, 115);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(100, 23);
+            this.txt_c.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 263);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.txt_f);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_f;
+        private System.Windows.Forms.TextBox txt_c;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fahrenheit_para_celsius
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txt_f.Text == "")
+            {
+                MessageBox.Show("PREENCHER O CAMPO", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else
+            {
+                //VARS
+                decimal fahrenheit = Convert.ToDecimal(txt_f.Text);
+                decimal celsius;
+
+                //CONVERSÃO
+
+                celsius = (fahrenheit - 32) * 5 / 9;
+
+                txt_c.Text = celsius.ToString("n1");
+
+            }
+
+
+
+
+        }
+    }
+}
